e21db69cffd7484c1bcc18fa9a337aa26cf85ebd
SirSpat <ptrakk@gmail.com>
2016-04-13 06:28:20 -0500

    updated version number
    to version 0.3 #movin on up! :100:

--- Auto-Reddit.user.js
+++ Auto-Reddit.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto-Reddit
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.reddit.com/*
